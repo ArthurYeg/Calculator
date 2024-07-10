345a59e7310cb0115b49e2d41feb36e7c34d374b
Артур <97597933+ArthurYeg@users.noreply.github.com>
2024-07-10 22:30:35 +0200

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calculator1 {
+public class Calculator {
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         String exp = scn.nextLine();
