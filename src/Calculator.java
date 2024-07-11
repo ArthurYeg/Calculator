aa16142309daa6ea74f6485ab772e6d287c2d76c
Артур <97597933+ArthurYeg@users.noreply.github.com>
2024-07-11 23:46:15 +0200

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class Calculator1 {
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         String exp = scn.nextLine();
         exp = exp.replace("\"", "");
         char action;
         String[] data;
+
         if (exp.contains("+")) {
             data = exp.split("\\+");
             action = '+';
@@ -22,45 +23,46 @@ public class Calculator {
         } else {
             throw new Exception("Некорректный знак действия");
         }
-        if (action == '*' || action == '/') {
-            if (data[1].contains("\"")) throw new Exception("Строку можно делить или умножать только на число");
-        }
+       
+        if (data[0].length() > 10) throw new Exception("Введено больше 10 символов в первой части");
+        if (data.length > 1 && data[1].length() > 10) throw new Exception("Введено больше 10 символов во второй части");
+
         for (int i = 0; i < data.length; i++) {
             data[i] = data[i].replace("\"", "");
         }
+
+        String result = "";
         if (action == '+') {
-            printInQuotes(data[0] + data[1]);
+            result = data[0] + data[1];
         } else if (action == '*') {
             int multiplier = Integer.parseInt(data[1]);
-            String result = "";
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < multiplier; i++) {
-                result += data[0];
+                sb.append(data[0]);
             }
-            printInQuotes(result);
+            result = sb.toString();
         } else if (action == '-') {
             int index = data[0].indexOf(data[1]);
             if (index == -1) {
-                printInQuotes(data[0]);
+                result = data[0];
             } else {
-                String result = data[0].substring(0, index);
-                result += data[0].substring(index + data[1].length());
-                printInQuotes(result);
+                result = data[0].substring(0, index) + data[0].substring(index + data[1].length());
             }
         } else {
-            int newLen = data[0].length() / Integer.parseInt(data[1]);
-            String result = data[0].substring(0, newLen);
-            printInQuotes(result);
+            int divisor = Integer.parseInt(data[1]);
+            if (divisor == 0) throw new Exception("Деление на ноль");
+            int newLen = data[0].length() / divisor;
+            result = data[0].substring(0, newLen);
         }
-        if (data[0].length() > 10)
-            throw new Exception("Введено больше 10 символов");
-        if (data[1].length() > 10)
-            throw new Exception("Введено больше 10 символов");
+
+        printInQuotes(result);
     }
 
-  public  static void printInQuotes(String text) {
-        if (text.length() > 40)
-            System.out.println(" " + text.substring(0, 40) + "..." + " ");
-        else
-            System.out.print(text);
+    public static void printInQuotes(String text) {
+        if (text.length() > 40) {
+            System.out.println("\"" + text.substring(0, 40) + "...\"");
+        } else {
+            System.out.println("\"" + text + "\"");
+        }
     }
 }
